45d6959c38e30a637e18b6c23f828befe3aee3ba
LeonAdeoye <leon.adeoye@gmail.com>
2022-04-23 23:10:15 +0800

    dockerfile

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 using Catalog.Repositories;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
-string CONNECTION_URI = "mongodb://woch_user:woch_user@leonadeoyemongodbcluster-shard-00-01-gni1u.azure.mongodb.net:27017,leonadeoyemongodbcluster-shard-00-00-gni1u.azure.mongodb.net:27017,leonadeoyemongodbcluster-shard-00-02-gni1u.azure.mongodb.net:27017/admin?serverSelectionTimeoutMS=20000&readPreference=primary&ssl=true";
+string CONNECTION_URI = "mongodb://w*ch_user:w*ch_user@leonadeoyemongodbcluster-shard-00-01-gni1u.azure.mongodb.net:27017,leonadeoyemongodbcluster-shard-00-00-gni1u.azure.mongodb.net:27017,leonadeoyemongodbcluster-shard-00-02-gni1u.azure.mongodb.net:27017/admin?serverSelectionTimeoutMS=20000&readPreference=primary&ssl=true";
 
 var builder = WebApplication.CreateBuilder(args);
 // Need to esnsure serialization in Mongo DB is string type for both GUID and DataTimeOffset.
@@ -12,6 +13,14 @@ BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 builder.Services.AddSingleton<IMongoClient>(serviceProvider => new MongoClient(CONNECTION_URI));
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
+
+// Added to do health checks on https://localhost:7071/health
+// You will also need a NugGet package: AspNetCore.HealthChecks.MongoDb to add a Mongo DB health check
+builder.Services.AddHealthChecks()
+    .AddMongoDb(CONNECTION_URI, 
+        name: "mongodb", 
+        timeout: TimeSpan.FromSeconds(20),
+        tags: new[] { "ready" });
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -30,5 +39,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Also needed for healthchecks.
+app.MapHealthChecks("/health/ready", new HealthCheckOptions { Predicate = (check) => check.Tags.Contains("ready")}); ;
+app.MapHealthChecks("/health/live", new HealthCheckOptions { Predicate = (_) => false }); ;
+
 
 app.Run();
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-dotnet new webapi -name catalog
+To create a new project solution run the command: dotnet new webapi -name catalog
+
+To check health use https://localhost:7170/health/
+To check health use https://localhost:7170/health/live
+To check health use https://localhost:7170/health/ready
+
+
+For Postman use https://localhost:7170/items
 
